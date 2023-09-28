87d1d04390e0fab1fcefdf2efd7175aa09209645
jnclement <jocl6895@colorado.edu>
2023-09-28 01:40:37 -0600

    trimmed down condor submission

--- build_condor.sh
+++ build_condor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 for ZCUT in 10 30; do #10 30
-    for SCALE in 1 1.1 1.3 1.5; do #1 1.3 1.5
-	for SUB in 0 0.01; do #0 0.018
-	    for MINE in 0 0.05 0.01 0.018; do #0 0.005 0.018
+    for SCALE in 1.3; do #1 1.3 1.5
+	for SUB in 0; do #0 0.018
+	    for MINE in 0 0.05; do #0 0.005 0.018
 		for SIM in 1; do
 		    for DAT in 1; do
-			for TAG in '_otc' '_ntc'; do
+			for TAG in '_ntc'; do
 			    NAME="condor_${ZCUT}${SCALE}${SUB}${MINE}${SIM}${DAT}${TAG}.sh"
 			    echo "#!/bin/bash" > $NAME
 			    echo "source /opt/sphenix/core/bin/sphenix_setup.sh -n" >> $NAME
