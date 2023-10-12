b2526d3f1fc247f07bb782e1dd90321c82fe341d
jocl <jocl6895@colorado.edu>
2023-10-11 20:40:47 -0600

    debug

--- build_hists.C
+++ build_hists.C
@@ -541,7 +541,7 @@ int build_hists(int simfrac = 1, int datfrac = 1, float zcut = 30, float simscal
 			  truthpar_et[j]->Fill(truthpar_eta[k],get_E_T_em(truthpar_e[k],truthpar_eta[k],0));
 			  if(!truthpar_hit->GetBinContent(truthpar_hit->GetBin(truthpar_eta[k])))
 			    {
-			      cout << truthpar_hit->GetBinContent(truthpar_hit->GetBin(truthpar_eta[k])) << endl;
+			      cout << i << " " << truthpar_hit->GetBinContent(truthpar_hit->GetBin(truthpar_eta[k])) << endl;
 			      truthpar_counts[j]->Fill(truthpar_eta[k]);
 			      truthpar_hit->Fill(truthpar_eta[k]);
 			    }
