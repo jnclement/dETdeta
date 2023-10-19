bcbd1d6d9be7ab040b94d1d59a4722954daaba88
jocl <jocl6895@colorado.edu>
2023-10-18 20:50:13 -0600

    Added +2 to scaling for nevt

--- build_hists.C
+++ build_hists.C
@@ -594,7 +594,7 @@ int build_hists(int simfrac = 1, int datfrac = 1, float zcut = 30, float simscal
 	{
 	  fullcor[j][i]->Divide(dETcent[1][j][i],dETcent[0][j][i]);
 	  fullcor[j][i]->Multiply(truthpar_et[i]);
-	  fullcor[j][i]->Scale(1./(nevt[1]/centbins));
+	  fullcor[j][i]->Scale(1./(nevt[1]/(centbins+2)));
 	  cout << dETcent[1][j][i]->GetBinContent(10) << " " << dETcent[0][j][i]->GetBinContent(10) << " " << truthpar_et[i]->GetBinContent(10) << " " << fullcor[j][i]->GetBinContent(10)<< " " << fullcor[j][i]->Integral()/(2*dETrange*175) <<endl;
 	  outf->WriteObject(fullcor[j][i],fullcor[j][i]->GetName());
 	}
