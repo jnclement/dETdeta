902279e5aee37721b3e7d7d6532b864ea970b908
jocl <jocl6895@colorado.edu>
2023-10-20 15:11:03 -0600

    needed width param

--- build_hists.C
+++ build_hists.C
@@ -604,9 +604,9 @@ int build_hists(int simfrac = 1, int datfrac = 1, float zcut = 30, float simscal
 	  fullcor[j][i]->Divide(dETcent[1][j][i],dETcent[0][j][i]);
 	  fullcor[j][i]->Multiply(truthpar_et[i]);
 	  fullcor[j][i]->Scale(1./(nevtcent[1][i]));
-	  cout << dETcent[1][j][i]->GetBinContent(10) << " " << dETcent[0][j][i]->GetBinContent(10) << " " << truthpar_et[i]->GetBinContent(10) << " " << fullcor[j][i]->GetBinContent(10)<< " " << fullcor[j][i]->Integral()/(2*dETrange*175) <<endl;
+	  cout << dETcent[1][j][i]->GetBinContent(10) << " " << dETcent[0][j][i]->GetBinContent(10) << " " << truthpar_et[i]->GetBinContent(10) << " " << fullcor[j][i]->GetBinContent(10)<< " " << fullcor[j][i]->Integral("width")/(2*dETrange*175) <<endl;
 	  outf->WriteObject(fullcor[j][i],fullcor[j][i]->GetName());
-	  if(i==centbins-1) cout << test << " " << dETcent[0][j][i]->Integral() << " " << dETcent[1][j][i]->Integral() << " " << fullcor[j][i]->Integral() << " " << truthpar_et[i]->Integral() << endl;
+	  if(i==centbins-1) cout << test << " " << dETcent[0][j][i]->Integral("width") << " " << dETcent[1][j][i]->Integral("width") << " " << fullcor[j][i]->Integral("width") << " " << truthpar_et[i]->Integral("width") << endl;
 	  cout << typeid(test).name() << typeid(truthpar_et[i]).name() << endl;
 	}
     }
