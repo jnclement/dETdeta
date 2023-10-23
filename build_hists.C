232134017d820bff5f0de9c03dee7c3dd815eb4b
jocl <jocl6895@colorado.edu>
2023-10-23 12:16:03 -0600

    no acceptance cut at all

--- build_hists.C
+++ build_hists.C
@@ -164,6 +164,7 @@ int set_cent_cuts(TH1* hist, float* cent, int centbins)
 
 int check_acceptance(int eta, int phi)
 {
+  return 0;
   if (eta < 9) return 1;
   if (phi > 200 || phi < 175) return 1;
   return 0;
