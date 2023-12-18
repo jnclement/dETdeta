7721f9f4cd02211b44fa8ff9fbbc10505711ae1d
Joseph Clement <jocl6895@colorado.edu>
2023-12-18 15:47:02 -0500

    changed scaling behavior

--- build_script.sh
+++ build_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for ZCUT in 30; do #10 30
-    for SCALE in 1.3; do #1 1.3 1.5
+    for SCALE in 1; do #1 1.3 1.5
 	for SUB in 0; do #0 0.018
 	    for MINE in 0; do #0 0.005 0.018
 		for SIM in 1; do
